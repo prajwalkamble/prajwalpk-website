240b508a1ba371bb1222fafb2bdac0133bbe1f35
Prajwal Kamble <65185064+prajwalkamble@users.noreply.github.com>
2025-01-19 01:34:25 +0530

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -3,9 +3,9 @@ const LocalConfig = {
     TZ: process.env.TZ,
     NODE_ENV: process.env.NODE_ENV,
     AUTOPREFIXER_GRID: process.env.AUTOPREFIXER_GRID,
-    NEXT_PUBLIC_GTAG_ID: process.env.NEXT_PUBLIC_GTAG_ID || "G-TNL2FC1PBE",
-    NEXT_PUBLIC_GITHUB_TOKEN: process.env.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,
-    NEXT_PUBLIC_VERCEL_TOKEN: process.env.a9MLGdi2fAY8eEGndAo6C8mX,
+    NEXT_PUBLIC_GTAG_ID: process.env.NEXT_PUBLIC_GTAG_ID,
+    NEXT_PUBLIC_GITHUB_TOKEN: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
+    NEXT_PUBLIC_VERCEL_TOKEN: process.env.NEXT_PUBLIC_VERCEL_TOKEN,
     NEXT_PUBLIC_RESUME_LINK: process.env.NEXT_PUBLIC_RESUME_LINK || "/Prajwal Kamble Resume.pdf",
   },
 };
